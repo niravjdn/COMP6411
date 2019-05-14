32db6b5f9b77dea378661fb70bc2f212932a3361
niravjdn <niravjdn@gmail.com>
2019-05-14 17:36:38 -0400

    Game Score Calculator Done

--- A1/Game.py
+++ A1/Game.py
@@ -1,10 +1,16 @@
 class Game:
+    frequency_list = [8.17, 1.49, 2.78, 4.25, 12.7, 2.23, 2.02, 6.09, 6.97, 0.15, 0.77, 4.03, 2.41, 6.75, 7.51, 1.93,
+                      0.1, 5.99, 6.33, 9.06, 2.76, 0.98, 2.36, 0.15, 1.97, 0.07]
+
+
     def __init__(self, i, word):
         self.count = i
         self.word = word
-
-    def set_bad_guess(self, bad_guess):
-        self.bad_guess = bad_guess
+        self.guess_attempt = 0
+        self.letter_attempt = 0
+        self.score = 0
+        self.missing_letters = 0
+        self.status = "Starting"
 
     def set_missing_letters(self, missing_letters):
         self.missing_letters = missing_letters
@@ -14,3 +20,38 @@ class Game:
 
     def set_status(self, status):
         self.status = status
+
+    def increase_guess_attemp(self):
+        self.guess_attempt = self.guess_attempt + 1;
+
+    def increase_letter_attempt(self):
+        self.letter_attempt = self.letter_attempt + 1
+
+    def calculate_final_score(self, temp, currentword, is_gave_up = False):
+        index_list = self.get_index_of_hidden_char(temp)
+        self.missing_letters = len(index_list)
+        # calculate for all hidden things first
+        for idx in index_list:
+            character = currentword[idx]
+            num = ord(character) - 97
+            self.score = self.frequency_list[num] + self.score
+
+        if(is_gave_up):
+            self.score = self.score * -1
+            return
+
+        # minus points for wrong guess and letter guess used
+        if self.letter_attempt != 0:
+            self.score = self.score / self.letter_attempt
+
+        self.score = self.score - (self.score * 0.10 * self.guess_attempt)
+
+        self.score = format(self.score, '.2f')
+
+    def get_index_of_hidden_char(self, temp):
+        index_list = list()
+        for idx, target in enumerate(temp):
+            if(target == "-"):
+                index_list.append(idx)
+        return index_list
+
--- A1/Guess.py
+++ A1/Guess.py
@@ -7,19 +7,23 @@ def guessWord(word_list):
     return word_list[random.randint(0,4029)]
 
 
-def printResults():
-    pass
+def printResults(game_list):
+    print("Game   Word      Status        Bad Guesses     Missing Letters    Score")
+    print("----   ----    -----------     ------------    ---------------    -----")
+    for game in game_list:
+        print(f"{game.count}      {game.word}      {game.status}            {game.guess_attempt}                  {game.missing_letters}           {game.score}")
 
 
-def handleSingleGuess(char, currentWord):
+def handleSingleGuess(char, temp, currentWord):
     print(f'You found {currentWord.count(char)} letters')
-    temp = currentWord
-    for c in temp:
-        if(c != char):
-            temp = temp.replace(c,'_')
+    for idx,c in enumerate(currentWord):
+        if(c == char):
+            temp = list(temp)
+            temp[idx] = char
+            temp = "".join(temp)
 
     print(f'Current Guess: {temp}')
-
+    return temp
 
 
 if __name__ == '__main__':
@@ -33,33 +37,59 @@ if __name__ == '__main__':
     count = 0
     game_over = False
     game_quit = False
-    game_list = []
-    while count < 100:
+    game_list = list()
+    while count < 2:
+        print("Starting")
         count = count + 1;
         currentWord = guessWord(word_list)
-        game_list.append(Game(count, currentWord))
+        currentWord='laid'
         print(currentWord)
+        temp = "----"
+        game = Game(count, currentWord)
+        print(len(game.frequency_list))
+        game_over = False
         while not game_over:
-            choice = input('g = guess, t = tell me, l for a letter, and q to quit')
+            choice = input('g = guess, t = tell me, l for a letter, and q to quit\n')
             #take in put and take action and update game model
             if (choice == 'g'):
-                guess_value = input("Enter Guess")
+                guess_value = input("Enter Guess:\n")
+
                 if(guess_value == currentWord):
-                    print('Congratulations, Starting new game')
+                    print('Congratulations, Starting new game\n')
+                    game.set_status("Success")
+                    game.calculate_final_score(temp, currentWord)
                     break
+                #only wrong guess attemp is counted
+                game.increase_guess_attemp()
             elif (choice == 't'):
                 print(f"The word is {currentWord}")
+                game.set_status("Gave up")
+                game.calculate_final_score(temp, currentWord, True)
                 break;
             elif (choice == 'l'):
-                char = input('Enter a letter')
-                handleSingleGuess(char, currentWord)
+                char = input('Enter a letter:\n')
+                temp = handleSingleGuess(char, temp, currentWord)
+                game.increase_letter_attempt()
+                #if all are discovered, mark its mark as zero
+                if("-" not in temp):
+                    game_over = True
+                    game.calculate_final_score(temp, currentWord)
             elif (choice == 'q'):
                 game_quit = True
                 break
             else:
                 print('Please Enter Valid Input \n')
 
+        game_list.append(game)
+
         if(game_quit):
             break
     
-    printResults()
+    printResults(game_list)
+
+def calculate_score(game):
+    pass
+
+
+
+

783eda1080433cc4a81507cfdee47b15922934d3
niravjdn <niravjdn@gmail.com>
2019-06-14 22:49:08 -0400

    ExucuteBank Function Called

--- Project-ER/src/banks.erl
+++ Project-ER/src/banks.erl
@@ -1,6 +1,6 @@
 % hello world program
 -module(banks). 
--export([start/0]). 
+-export([start/0, executeBank/3]). 
 
 start() ->
 	{ok, Customers} = file:consult("customers.txt"),
@@ -14,13 +14,20 @@ start() ->
 	F2_list = maps:from_list(Customers),
     Method_ID = self(),
 	% iterate hashmap
- 	ets:new(table_1, [named_table, protected, set, {keypos, 1}]),
+ 	%ets:new(table_1, [named_table, protected, set, {keypos, 1}]),
 	maps:fold(fun(K, V, ok) ->
-   	 	ets:insert(table_1, {a,K}),
+%%    	ets:insert(table_1, {a,K}),
+		P_ID = spawn (banks, executeBank, ['rbc', F_list, K]),
+%% call usin P_ID ! {} something like this 
+		io:format("~w: PID ~n",[P_ID]),
 		io:format("~w: this  ~w.~n",[K,V])
 		end,ok, F_list),
-		ok.
+	io:format("finalll ~w",[F_list]),
+	ok.	
 
 executeBank(Bank, MapOfCustomer, CurrentCustomer) ->
-	done.
+ 	io:format("abc ~w",[MapOfCustomer]),
+%% add receive quote here 	
+	maps:put("a", 42, MapOfCustomer).
+	
 	

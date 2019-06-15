cd0a72813c08ba52ef414d51d98419eb62c27934
niravjdn <niravjdn@gmail.com>
2019-06-15 19:57:21 -0400

    Customer and bank Process

--- Project-ER/src/bank.erl
+++ Project-ER/src/bank.erl
@@ -4,25 +4,23 @@
 
 -module(bank).
 
--export([performBankOperation/5]).
+-export([performBankOperation/2]).
 
 % bank functions here like updating balance and updating user
 
-performBankOperation(CurrentCustomer, Randombank, RandomAmount, MapOfCustomer, BankList) ->
-	[{Name,Req}] = ets:lookup(table_2, CurrentCustomer),
-	[{Bank,Bal}] = ets:lookup(table_3, Randombank),
-	if
-		RandomAmount >= Bal -> 
- 			io:format("~w denies a loan of ~w dollars from ~w ~n",[Bank, RandomAmount, Name]);
-		true ->
-			io:format("~w approves a loan of ~w dollars from ~w ~n",[Bank, RandomAmount, Name]),
-			% do stuff and update table
-			Bal2 = Bal - RandomAmount,
-			Req2 = Req - RandomAmount,
-			ets:insert(table_2, {Name,Req2}),
-			ets:insert(table_3, {Bank,Bal2}),
-			P_ID = spawn (customer, receviedForCustomer, [1]),
-			P_ID ! {approved,CurrentCustomer, Bank, RandomAmount, MapOfCustomer, BankList}
-			% call receive
-			% call customer receive
+performBankOperation(Bank, Bal) ->
+	receive
+		{From, {debitFromBank, RandomAmount}} ->
+			if
+				RandomAmount > Bal -> 
+%%  					io:format("~w denies a loan of ~w dollars from ~w ~n",[Bank, RandomAmount, Name]);
+						From ! {self(), {denyFromBank, Bank}},
+						io:format("");
+				RandomAmount =< Bal ->
+%% 						io:format("~w approves a loan of ~w dollars from ~w ~n",[Bank, RandomAmount, Name]),
+						Bal2 = Bal - RandomAmount,
+						From ! {self(), {acceptFromBank, RandomAmount}},
+						performBankOperation(Bank, Bal2)
+			end
 	end.
+	
--- Project-ER/src/customer.erl
+++ Project-ER/src/customer.erl
@@ -53,6 +53,50 @@ executeCustomer(MapOfCustomer, CurrentCustomer, BankList) ->
 			spawn (bank, performBankOperation, [CurrentCustomer, Randombank, RandomAmount, MapOfCustomer, BankList])			
 	end.
 
+
+createCustomer(CustomerName, Bal, BankList) ->
+    receive
+        {From, {requestBank}} ->
+			LengthOfList = length(BankList),
+			if 
+			Bal >= 50 -> 
+				RandomAmount = rand:uniform(50);
+			true ->
+				RandomAmount = rand:uniform(Bal)
+			end,
+		
+			if
+			  	(Bal >= 0) and (LengthOfList > 0) -> 
+					Index = rand:uniform(length(BankList)),
+					io:format("Index ~w ~n",[Index]),
+					Randombank = nth(Index,BankList),
+					io:fwrite("~p~n", Randombank),
+					%now call bank function and wait for receive to call this another time or quit
+					SleepTime = rand:uniform(90) + 10,
+					timer:sleep(SleepTime),
+					%call bank process that performs operation on random bank and currentCustomer
+					% directly call bank process by name
+				    % update balance
+					Balance2  = Bal			
+			end,
+			createCustomer(CustomerName, Balance2, BankList);
+           
+		{From, {denyFromBank, BankName}} ->
+			LengthOfList = length(BankList),
+			if 
+				LengthOfList >= 2->
+				createCustomer(CustomerName, Bal, lists:delete(BankName, BankList))
+			end;
+			
+		{From, {acceptFromBank, AmountDebited}} ->
+			createCustomer(CustomerName, Bal - AmountDebited, BankList);
+			
+		  %print
+        terminate ->
+            ok
+    end.
+
+
 %BankList is a local list of bank for particular customer.
 %% call bank here until some point and then process called here will call receivedForCustomer
 
